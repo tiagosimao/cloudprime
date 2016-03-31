5f32cb8bc78d7de3514054be09dc4e7263b06738
UTurista <vascko@sapo.pt>
2016-03-31 12:30:27 +0100

    Metric class now Logs metrics to a file (just in case)

--- webserver/src/main/java/org/irenical/ist/cnv/cloudprime/stats/Metric.java
+++ webserver/src/main/java/org/irenical/ist/cnv/cloudprime/stats/Metric.java
@@ -1,23 +1,37 @@
 package org.irenical.ist.cnv.cloudprime.stats;
 
+import java.io.IOException;
 import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Metric {
+    private static Logger mLog;
     private static int mActiveThreads = 0;
     private static Map<Long, Stat> mStats = new HashMap<Long, Stat>();
+
+    static {
+        mLog = Logger.getLogger(Metric.class.getName());
+        try {
+            mLog.addHandler( new FileHandler("metrics.log"));
+        } catch (IOException e) {
+            mLog.log(Level.SEVERE, e.getMessage());
+        }
+    }
 
     public static synchronized void start(BigInteger bigInteger) {
         mActiveThreads++;
         mStats.put(Thread.currentThread().getId(), new Stat(bigInteger));
-        System.out.println(String.format("Factoring: %d (%d/%d active threads)", bigInteger, mActiveThreads, Thread.activeCount()));
+        mLog.log(Level.INFO, String.format("Factoring: %d (%d/%d active threads)", bigInteger, mActiveThreads, Thread.activeCount()));
     }
 
     public static synchronized void end() {
         mActiveThreads--;
         Stat stat = mStats.get(Thread.currentThread().getId());
-        System.out.println(String.format("Factorization of: %d took %d instructions and %d methods", stat.TargetNumber, stat.InstructionsCounter, stat.MethodCounter));
+        mLog.log(Level.INFO, (String.format("Factorization of: %d took %d instructions and %d methods", stat.TargetNumber, stat.InstructionsCounter, stat.MethodCounter)));
 
         // TODO: save the Stats information to a permanent database
     }
