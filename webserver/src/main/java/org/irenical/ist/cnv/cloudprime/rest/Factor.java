8ad8b25e7c4c70f4d6ddb3c53a0e421ee7f2ba6f
UTurista <vascko@sapo.pt>
2016-04-30 12:44:41 +0100

    Rest /Factor is now Non-Blocking, fixes slow HealthChecks

--- webserver/src/main/java/org/irenical/ist/cnv/cloudprime/rest/Factor.java
+++ webserver/src/main/java/org/irenical/ist/cnv/cloudprime/rest/Factor.java
@@ -8,6 +8,8 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -17,23 +19,19 @@ import org.irenical.ist.cnv.cloudprime.stats.Metric;
 @Path("f.html")
 public class Factor {
 
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response postNumber(@QueryParam("n") String number) {
-        return handleNumber(new BigInteger(number));
-    }
-
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getNumber(@QueryParam("n") String number) {
-        return handleNumber(new BigInteger(number));
-    }
+    public void asyncGet(@Suspended final AsyncResponse asyncResponse, @QueryParam("n") final String number) {
 
-    private Response handleNumber(BigInteger integer) {
-        Metric.start(integer);
-        ArrayList<BigInteger> res = new IntFactorization().calcPrimeFactors(integer);
-        Metric.end();
-        return Response.ok(res).build();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                BigInteger integer = new BigInteger(number);
+                Metric.start(integer);
+                ArrayList<BigInteger> res = new IntFactorization().calcPrimeFactors(integer);
+                Metric.end();
+                asyncResponse.resume(res);
+            }
+        }).start();
     }
-
 }
